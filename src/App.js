38af7455a59393495354922b23964cb0cd0fa27a
xavier-kong <kongxianchengxavier@gmail.com>
2022-06-06 19:46:07 +0800

    completed up to 6:03 of tutorial 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div class="shrink-0">
+    </div>
+    <div>
+      <div class="text-xl font-medium text-black">ChitChat</div>
+      <p class="text-slate-500">You have a new message!</p>
+    </div>
+  </div>
   )
 }
