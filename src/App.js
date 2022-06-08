5ccdf20b43211eefb86f47901f22c0f2a4b2ad58
xavier-kong <kongxianchengxavier@gmail.com>
2022-06-08 20:46:34 +0800

    completed tutorial 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 export default function App() {
   return (
-    <div className="container">
-      <TailwindCSSButton />
+    <div className="container p-4">
+      <TailwindCSSButton>
+        TailwindCSS
+      </TailwindCSSButton>
   </div>
   )
 }
 
-function TailwindCSSButton() {
+function TailwindCSSButton({ children, href }) {
   return (
-    <button className="bg-blue-500">testing button</button>
+    <a href={href} className="bg-blue-500 text-white font-medium px-4 py-2 rounded hover:bg-blue-600">{children}</a>
   )
 }
