f7a6a53688be692a48ae150efedac1d1bbddb766
xavier-kong <kongxianchengxavier@gmail.com>
2022-06-11 15:56:53 +0800

    comppleted up to 7:53

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,6 +1,6 @@
 export default function SideBar () {
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow">
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary">
             <i>A</i>
             <i>B</i>
             <i>C</i>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   content: [
@@ -23,7 +25,18 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#ff4800"
+        primary: '#202225',
+        secondary: '#5865f2',
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+      },
       }
     },
   },
