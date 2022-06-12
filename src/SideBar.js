a667a975b8c8fb425117e1185c82f24ed6ec327a
xavier-kong <kongxianchengxavier@gmail.com>
2022-06-12 23:37:02 +0800

    completed up to 8:13

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,3 +1,6 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
 export default function SideBar () {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary">
