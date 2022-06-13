ef7c2b6c7b4692987b05857165b460df04949b17
xavier-kong <kongxianchengxavier@gmail.com>
2022-06-13 19:42:49 +0800

    completed up to 8:25

--- src/SideBar.js
+++ src/SideBar.js
@@ -4,11 +4,10 @@ import { FaFire, FaPoo } from 'react-icons/fa';
 export default function SideBar () {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary">
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+            <SideBarIcon icon={<FaFire size="28" />} />
+            <SideBarIcon icon={<BsPlus size="32" />} />
+            <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+            <SideBarIcon icon={<FaPoo size="20" />} />
         </div>
     )
 }
